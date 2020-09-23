8b6b7a8e5b48d1d3bc53d8c84c0fd808fe7d347a
Bryan Lu <Byl222@lehigh.edu>
2020-09-23 15:23:31 -0400

    Completed Assignment 2

--- Assignment 2.R
+++ Assignment 2.R
@@ -10,17 +10,15 @@ only_pa <- mutate(only_pa, adj_deaths = deaths)
 
 ##April 21st, 2020 deaths should be adjusted downward by 282
 
-#mutate(only_pa, adj_deathsd)
-
-#n<-length(only_pa)
-
-#for (i in 1:n) {
-#  if(only_pa[i]=="x"){
-#    print(age[i])
-#  } else{}
-#}
-
 ##April 22nd, 2020 deaths should be adjusted downward by 297
+
+n<-length(only_pa$date)
+
+for (i in 1:n) {
+  if (as.character(only_pa$date[i])=="2020-04-21") {
+    only_pa$adj_deaths[i] <- (only_pa$deaths[i] - 282)}
+   else if(as.character(only_pa$date[i])=="2020-04-22") {
+     only_pa$adj_deaths[i] <- (only_pa$deaths[i] - 297)}}
 
 ##Calculate sum of all adj_deaths in PA and Print to console
 sum(only_pa$adj_deaths)
