50d29bc47733513ac7cdd2b497b5e1037081eed7
Bryan Lu <Byl222@lehigh.edu>
2020-10-08 17:23:51 -0400

    Assignment 4

---
+++ Assignement 4.R
@@ -0,0 +1,33 @@
+#Assignment 4 by Bryan Lu
+
+library(tidyverse)
+library(scales)
+
+gamers <- read.csv("Most Popular Twitch Streamers.csv")
+str(gamers)
+gamers$Date <- as.Date(gamers$Date, "%m/%d/%Y")
+#View(gamers)
+
+
+p <- ggplot(as.data.frame(gamers),
+            mapping = aes(x = Date,
+                          y = Viewers,
+                          color = Streamer)) 
+
+p + geom_line(mapping= aes(group= Streamer)) +
+  scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6))+
+  labs(title= "Most Popular Twitch Streamers")
+
+###second graph
+
+p <- ggplot(as.data.frame(gamers),
+       mapping = aes(x = Date,
+                     y = Viewers,
+                     color = Streamer)) 
+  p + geom_line(mapping= aes(group= Streamer))+
+    facet_wrap(~Streamer)+
+      geom_line()+facet_wrap(~Streamer)+
+      scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6))+
+      labs(title= "Most Popular Twitch Streamers")
+      
+
